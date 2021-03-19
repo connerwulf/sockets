c54c2ff5d1be1faae97b15dd0109e701f2996181
Conner <connerwulf@usf.edu>
2021-03-18 21:22:04 -0400

    tess

--- sockets.c
+++ sockets.c
@@ -14,7 +14,7 @@
 //#define PORT 1050 //FOR TURN IN
 #define PORT 1079
 #define SERVER_IP "131.247.3.8"
-#define BUFFERSIZE 16
+#define BUFFERSIZE 15
 char sharedArray[BUFFERSIZE];
 sem_t semaphore;
 
